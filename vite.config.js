a928f8160969579161f6f120d286f94d4a87eae6
sheida <89011203+SheidaSepehri01@users.noreply.github.com>
2023-01-29 12:20:39 +0330

    edited setings for gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 //https://vitejs.dev/config/
 export default defineConfig({
+  base:'https://sheidasepehri01.github.io/traveling/',
   plugins: [react()],
   define: {
     "global": {},
